3ba9b65b61a31cf61bcdaa3f6d3042482a6fde69
TiernanMcCarthy <tiernanmccarthy@outlook.com>
2021-06-16 14:25:13 +0100

    Spawn Management in place randomisation needs to be added to stats

--- BarTime/Assets/Scripts/AI/ManagerMan.cs
+++ BarTime/Assets/Scripts/AI/ManagerMan.cs
@@ -6,9 +6,117 @@ public class ManagerMan : MonoBehaviour
 {
     public List<Punter> punters;
 
+    public int NumberOfPunters = 0;
 
     public Exit Exit;
 
     public float TotalMoney;
+
+    public Punter Prefab;
+
+
+    public int MaximumPunters = 20;
+
+    public int WaveSize = 5;
+
+    public float TimeBetweenWaves = 15;
+
+    private float LastWaveTime = 0;
+
+
+    public List<Transform> Spawns;
+
+    public List<Transform> MoveToLocations;
+
+
+
+    private void RandomisePunter(Punter Edit)
+    {
+
+    }
+
+
+    private void SpawnPrefab()
+    {
+        Punter Temp = Instantiate(Prefab, Spawns[Random.Range(0, Spawns.Count)].position,Quaternion.identity);
+
+        int Case = Random.Range(0, 2);
+
+        if(Case==0)
+        {
+            Temp.Team = false;
+        }
+        else
+        {
+            Temp.Team = true;
+        }
+
+        Temp.MoveToLocation = Spawns[0].position; //Address
+
+        punters.Add(Temp);
+
+
+    }
+
+
+    private void SpawnPunters(int Remainder)
+    {
+
+
+       
+
+
+        for(int i=NumberOfPunters; i<MaximumPunters; i++)
+        {
+            Remainder -= 1;
+            SpawnPrefab();
+            NumberOfPunters++;
+            if (Remainder == 0) //Stop spawning them
+            {
+                break;
+            }
+        }
+
+
+    }
+
+
+    private void CheckStateOfPunters()
+    {
+        if(NumberOfPunters<MaximumPunters && Time.time-LastWaveTime>=TimeBetweenWaves) //If there is room to spawn punters
+        {
+
+            SpawnPunters(MaximumPunters - NumberOfPunters);
+            LastWaveTime = Time.time;
+
+        }
+    }
+
+
+    void CheckPuntersList()
+    {
+        punters = new List<Punter>();
+        Punter[] temp = FindObjectsOfType<Punter>();
+
+        foreach(Punter p in temp)
+        {
+            punters.Add(p);
+        }
+        NumberOfPunters = punters.Count;
+    }
+
+    public void Start()
+    {
+        SpawnPrefab();
+    }
+
+    public void FixedUpdate()
+    {
+        CheckPuntersList();
+        if(Input.GetKeyDown("a"))
+        {
+            SpawnPrefab();
+        }
+    }
 
 }
