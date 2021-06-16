573f28983bbb06dde4ebaef32f237ad2858a8ee2
TiernanMcCarthy <tiernanmccarthy@outlook.com>
2021-06-16 18:11:14 +0100

    Lemon Squeazy

--- BarTime/Assets/Scripts/Character.cs
+++ BarTime/Assets/Scripts/Character.cs
@@ -104,6 +104,19 @@ public class Character : MonoBehaviour
             CurrentCheese = null;
         }
     }
+
+    public struct CheeseStacky
+    {
+        public int Index;
+        public CheeseStack Sample;
+        public CheeseStacky(int ind, CheeseStack s)
+        {
+            Sample = s;
+            Index = ind;
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -111,7 +124,51 @@ public class Character : MonoBehaviour
 
         if(Input.GetKeyDown("e"))
         {
-            GiveCheese();
+            if (CurrentCheese != null)
+            {
+                GiveCheese();
+            }
+            else
+            {
+                Physics.OverlapSphere(transform.position,2.0f);
+                Collider[] hitList = Physics.OverlapSphere(transform.position, 30);
+                List<CheeseStacky> ClosestPositions = new List<CheeseStacky>();
+
+                for (int i = 0; i < hitList.Length; i++)
+                {
+                    if (hitList[i].gameObject != gameObject && hitList[i].gameObject.GetComponent<CheeseStack>())
+                    {
+                            CheeseStacky Close = new CheeseStacky(i, hitList[i].gameObject.GetComponentInParent<CheeseStack>());
+                            ClosestPositions.Add(Close);
+                        
+                    }
+                }
+                if (ClosestPositions.Count != 0)
+                {
+
+                    if (ClosestPositions.Count > 1)
+                    {
+                        for (int i = 0; i < ClosestPositions.Count - 1; i++) //Iterate through these and find the closest position by sorting
+                        {
+
+
+                            if (Vector3.Distance(ClosestPositions[i].Sample.transform.position, transform.position) > Vector3.Distance(ClosestPositions[i + 1].Sample.transform.position, transform.position))
+                            {
+                                CheeseStacky temp = ClosestPositions[i];
+                                ClosestPositions[i] = ClosestPositions[i + 1]; //Swap these positions
+                                ClosestPositions[i + 1] = temp;
+                                i = -1; //Reiterate
+                            }
+
+                        }
+                    }
+                    // SetVehicle(hitList[ClosestPositions[0].Index].gameObject); //set the target vehicle
+                    //MainActor.Target = ClosestPositions[0].Sample;
+                   // return Vector3.Distance(ClosestPositions[0].Sample.transform.position, MainActor.transform.position);
+                   //ClosestPositions[0].Sample
+                }
+
+            }
         }
 
         if(CurrentCheese!=null)
