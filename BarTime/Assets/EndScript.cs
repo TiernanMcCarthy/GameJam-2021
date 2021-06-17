d6648c2f741b38f976bab499933ecb882513220e
TiernanMcCarthy <tiernanmccarthy@outlook.com>
2021-06-17 18:36:49 +0100

    Music and Menus

---
+++ BarTime/Assets/EndScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
