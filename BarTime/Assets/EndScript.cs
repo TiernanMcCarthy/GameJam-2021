b25b3000856fff0d0e61893cacd1fde7fb2f888f
TiernanMcCarthy <tiernanmccarthy@outlook.com>
2021-06-17 19:09:27 +0100

    Ending added

--- BarTime/Assets/EndScript.cs
+++ BarTime/Assets/EndScript.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class EndScript : MonoBehaviour
 {
 
+    public ManagerMan Scene;
+
+    public TMPro.TextMeshProUGUI Money;
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void Start()
+    {
+        Scene = FindObjectOfType<ManagerMan>();
+
+        Money.text = "Tips Recieved: " +Scene.TotalMoney.ToString();
+        
     }
 
 
